c3b4d83942ef930621bf4a33d931b9979e8d35ae
Favour Edet <phavourfreeze@gmail.com>
2022-12-09 07:58:22 -0800

    print c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++);
 		putchar(c);
-	for (c + 'A'; c <= 'Z'; c++)
-		putchar(c)
+	for (c + 'A'; c <= 'Z'; c++);
+		putchar(c);
 	putchar('\n');
 	return (0);
 }
